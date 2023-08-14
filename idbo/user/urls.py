e7c162c30174abd9c7d32f9be1bd62f4cbffd22a
LeatherWoman <83293063+LeatherWoman@users.noreply.github.com>
2023-08-14 10:06:33 +0300

    Update urls.py

--- idbo/user/urls.py
+++ idbo/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from django.conf import settings
-from .views import StudentLoginView
+from .views import CustomTokenObtainPairView,CheckUser
 
 urlpatterns = [
     # ...
-    path('login/', StudentLoginView.as_view(), name='student-login'),
+    #path('login/', StudentLoginView.as_view(), name='student-login'),
+    path("auth", CustomTokenObtainPairView.as_view()),
+    path("check-user",CheckUser.as_view()),
     # ...
 ]
 
