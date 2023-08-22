9a875bd933318d7de6d459adab215b78c838bbe0
LeatherWoman <83293063+LeatherWoman@users.noreply.github.com>
2023-08-22 21:17:26 +0300

    Update settings.py

--- idbo/idbo/settings.py
+++ idbo/idbo/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'idbo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'launcher/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
